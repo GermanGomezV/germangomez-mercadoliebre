7d02266fec906d8b21747e90af7c3f5bad518741
GermanGomezV <ger_7214@hotmail.com>
2021-05-03 17:56:30 -0300

    Actualizando Server

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+
+/*Cinfiguraciones*/
 app.use(express.static('public'));
 
-
-app.listen(3000, ()=>{
+/*Server*/
+app.listen(process.env.PORT || 3000, function (){
     console.log('Servidor funcionando en el puerto 3000');
 });
 
+/*Rutas*/
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
